dc06a14e749afb0b5133f084a5da252843db6774
Ebrahim Tayabali <ebrahimhakimuddin@gmail.com>
2025-08-30 23:04:44 +0300

    fix: Initialize OneSignal with appId in the ready function

--- onesignal_erpnext/public/js/onesignal.js
+++ onesignal_erpnext/public/js/onesignal.js
@@ -3,7 +3,11 @@ import("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js");
 window.OneSignalDeferred = window.OneSignalDeferred || [];
 
 $(document).ready(() => {
+ 
   OneSignalDeferred.push(async function (OneSignal) {
+     await OneSignal.init({
+         appId: "YOUR_ONESIGNAL_APP_ID",
+       });
     if (frappe.session.user === "Guest") {
       await OneSignal.logout();
     } else {
