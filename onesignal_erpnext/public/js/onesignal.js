ede85b38db9a63457222be0b0b378a0c5010c7e1
Ebrahim Tayabali <ebrahimhakimuddin@gmail.com>
2025-09-09 10:13:54 +0300

    fix: Add manifest link creation in OneSignal initialization

--- onesignal_erpnext/public/js/onesignal.js
+++ onesignal_erpnext/public/js/onesignal.js
@@ -3,7 +3,10 @@ import("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js");
 window.OneSignalDeferred = window.OneSignalDeferred || [];
 
 $(document).ready(() => {
- 
+const link = document.createElement("link");
+link.rel = "manifest";
+link.href = "/files/manifest.json"; // change path if needed
+document.head.appendChild(link); 
   OneSignalDeferred.push(async function (OneSignal) {
      await OneSignal.init({
          appId: "YOUR_ONESIGNAL_APP_ID",
